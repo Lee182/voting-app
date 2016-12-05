9f49b87e79975213e0b6faa117d8866f2d14ff05
Jonathan T L Lee <jono-lee@hotmail.co.uk>
2016-12-05 17:14:37 +0000

    tests for dao

---
+++ tests/test1.js
@@ -0,0 +1,41 @@
+dao = require('../dao/index.js')
+function log(str) {
+  return function(a){
+    console.log(str, a)
+    return Promise.resolve(a)
+  }
+}
+
+Promise.resolve().then(function(){
+
+  return dao.connect()
+
+}).then(function(){
+
+  return dao.db.collection('polls').remove({})
+
+}).then(function(){
+  return dao.poll_create({
+    poll: {
+      question: 'Whats your favourite color',
+      user_id: 'dave',
+      creation_date: new Date()
+    }
+  })
+  .then(log('pol_create: res:'))
+  .catch(log('pol_create: err:'))
+
+}).then(function(res){
+
+  return dao.poll_option_add({
+    option: {
+      option: 'blue',
+      user_id: 'dave',
+      creation_date: new Date()
+    },
+    poll_id: res.result._id
+  })
+  .then(log('poll_option_add: res:'))
+  .catch(log('poll_option_add: err:'))
+
+})
