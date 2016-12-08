7f288e3650ba429a697cb47e697369fb3b2f53e5
Jonathan T L Lee <jono-lee@hotmail.co.uk>
2016-12-08 20:30:41 +0000

    moved file

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var app = require('express')()
-var server = require('http').Server(app)
+var express = require('express')
+var app = express()
+var http = require('http')
+var server = http.Server(app)
 var io = require('socket.io')(server)
 var port = process.env.PORT || 3000
 
