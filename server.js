668c2540afaf81ca50a56dad307906625211d043
Jonathan T L Lee <jono-lee@hotmail.co.uk>
2016-12-08 20:26:51 +0000

    added in socket.io setup

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-const express = require('express')
-const app = express()
+var app = require('express')()
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
 var port = process.env.PORT || 3000
+
+io.on('connection', function (socket) {
+  console.log(client.headers['cookie'])
+  socket.emit('news', { hello: 'world' })
+  socket.on('my other event', function(data) {
+    console.log(data)
+  })
+})
 
 app.use('/', express.static(__dirname + '/dist'))
 
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('server listening at http://localhost:'+port)
 })
