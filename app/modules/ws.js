0deb88f56d5b863a7242f8b6080e879cb08b3375
Jonathan T L Lee <jono-lee@hotmail.co.uk>
2017-01-22 09:25:18 +0000

    ws_run, req res method that returns a promise

--- app/modules/ws.js
+++ app/modules/ws.js
@@ -1,6 +1,6 @@
 io = require('socket.io-client')
 
-module.exports = function({data, watch, computed}){
+module.exports = function({data, methods, computed}){
   data.ws_connected = false
   data.ws_clients_count = 0
 
@@ -21,15 +21,36 @@ module.exports = function({data, watch, computed}){
   ws.on('ws_clients_count', function(e){
     data.ws_clients_count = e
   })
-
-  ws.on('run', function(res){
-    console.log('ws run', res)
-  })
   w.on('unload', function(){
     // when browser closing tab
     ws.disconnect()
   })
-  return ws
+
+  var _ws_run_reqs = {}
+  var _ws_run_resolutions = {}
+  function randstr_id(){
+    return Date.now()+'-'+Math.floor(Math.random()*100000)
+  }
+  methods.ws_run = function(o) {
+    return new Promise(function(resolve){
+      o.reqtoken = randstr_id()
+      _ws_run_reqs[o.reqtoken] = o
+      _ws_run_resolutions[o.reqtoken] = resolve
+      ws.emit('run', o)
+    })
+  }
+  ws.on('run', function(res){
+    var req = _ws_run_reqs[res.reqtoken]
+    var resolution = _ws_run_resolutions[res.reqtoken]
+    if (req === undefined && resolution === undefined) {return}
+
+    resolution({req, res, yay: 'yaya1!!'})
+
+    delete _ws_run_reqs[res.reqtoken]
+    delete _ws_run_resolutions[res.reqtoken]
+  })
+
+  w.ws = ws
 }
 
 
@@ -38,6 +59,19 @@ module.exports = function({data, watch, computed}){
 
 
 /*
+
+vm.ws_run({
+  cmd: 'poll_create',
+  data: {
+    poll: {
+      question: 'Whats your favourite color',
+      user_id: 'dave',
+      options: ['red', 'yellow', 'green']
+    }
+  }
+}).then(function(a){
+  console.log(a)
+})
 
 // create poll
 ws.emit('run', {
