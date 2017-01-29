6cb64b8d93baf4957f2990134e6a288bd206f70f
Jonathan T L Lee <jono-lee@hotmail.co.uk>
2017-01-29 12:42:16 +0000

    added voted! indicator in top right of poll view

--- app/modules/poll_view.js
+++ app/modules/poll_view.js
@@ -1,16 +1,30 @@
 var fn_chartmap = require('./fn_chartmap.js')
 module.exports = function({data, methods}) {
-  data.polls = []
-  data.poll_charts = {}
+  data.polls = {}
   data.poll_view__anom_voters_view = 'all'
+  data.polls_inview = []
   methods.poll_view__reset = function() {
     this.polls = []
+    this.poll_view__anom_voters_view = 'all'
+  }
+
+  methods.poll_view__update = function() {
+    let vm = this
+    vm.polls_inview = Object.keys(vm.polls).map(function(id){
+      return vm.polls[id].poll
+    })
   }
 
   methods.poll_view__vote_check = function(poll, option) {
     let vm = this
     const prop = vm.user_id === undefined ? 'ip' : 'user_id'
     return poll.votes[prop].option === option
+  }
+
+  methods.poll_view__voted = function(poll) {
+    let vm = this
+    var prop = vm.user_id === undefined ? 'ip' : 'user_id'
+    return poll.votes[prop].option !== undefined
   }
 
   methods.poll_view__vote_tick = function(poll, option) {
@@ -32,7 +46,6 @@ module.exports = function({data, methods}) {
     vm.$forceUpdate()
   }
 
-
   methods.poll_view__vote_cast = function(poll) {
     let vm = this
     const prop = vm.user_id === undefined ? 'ip' : 'user_id'
@@ -51,18 +64,34 @@ module.exports = function({data, methods}) {
       poll_id: poll.id
     }}).then(function(o){
       console.log(o)
-      var i = vm.polls.findIndex(function(poll){
-        return poll.id === o.res.poll.id
-      })
-      vm.polls[i] = o.res.poll
+      vm.poll_view__addpoll(o.res.poll)
       vm.$forceUpdate()
     })
   }
 
   methods.poll_view__addpoll = function(poll){
     let vm = this
-    vm.polls.unshift( poll )
+    if (vm.polls[poll.id] === undefined) {
+      vm.polls[poll.id] = {
+        poll: {},
+        chart: {},
+        showresults: false,
+        resultsview: 'n' // pie
+      }
+    }
+    vm.polls[poll.id].poll = poll
     vm.poll_view__chartmap( poll )
+    vm.poll_view__update()
+  }
+
+  methods.poll_view__showresults = function(poll_id, bool) {
+    let vm = this
+    console.log(poll_id, bool)
+    vm.polls[poll_id].showresults = Boolean(bool)
+    if (bool === 'pie' || bool === 'n') {
+      vm.polls[poll_id].resultsview = bool
+    }
+    vm.$forceUpdate()
   }
 
   methods.poll_view__anom_voter_tog = function() {
@@ -74,14 +103,14 @@ module.exports = function({data, methods}) {
       vm.poll_view__anom_voters_view = 'all'
     }
     vm.$forceUpdate()
-    // Object.keys(vm.poll_charts).forEach(function(id){
-    // })
+    Object.keys(vm.polls).forEach(function(id){
+      vm.poll_view__chartmap(vm.polls[id].poll)
+    })
   }
 
   methods.poll_view__chartmap = function(poll) {
     let vm = this
     var chart = fn_chartmap(poll, vm.poll_view__anom_voters_view)
-    console.log('chart',chart)
-    vm.poll_charts[poll.id] = chart
+    vm.polls[poll.id].chart = chart
   }
 }
