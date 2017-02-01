44bd3c25b1440b5915e503626420d55868ed95ad
Jonathan T L Lee <jono-lee@hotmail.co.uk>
2017-02-01 10:08:29 +0000

    removal unnneccessary modules

--- app/index.js
+++ app/index.js
@@ -5,25 +5,25 @@ w.postJSON = require('./lib/postJSON.js')
 
 // var poll_example = require('../example_json/poll_many-votes.js')
 var vue_charts = require('vue-charts')
-
 Vue.use(vue_charts)
+
 // module loading
 w.modules = {
   poll_create: require('./modules/poll_create.js'),
   poll_view: require('./modules/poll_view.js'),
   header_message: require('./modules/header_message.js'),
-  ws: require('./modules/ws.js'),
-  datas: require('./modules/$data.js'),
+  ws: require('./modules/ws.js')
+  // datas: require('./modules/$data.js'),
 }
 
 vueobj = {
   el: '#app',
   data: {
-    user_id: 'davee'
+    user_id: undefined,
+    ip: undefined
   },
   computed: {},
   watch: {},
-
   methods: {},
 
   // https://vuejs.org/v2/guide/instance.html#Lifecycle-Diagram
